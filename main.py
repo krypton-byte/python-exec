7029d6aac1a47482331d8bb5bf2b2873ae0cb9f7
krypton-byte <rosid6434@gmail.com>
2020-08-26 18:54:09 +0700

    wah

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import *
+import sys
+from io import StringIO
+import contextlib
+app = Flask(__name__)
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    old = sys.stdout
+    if stdout is None:
+        stdout = StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old
+
+@app.route('/',methods=['GET','POST'])
+def index():
+	try:
+		with stdoutIO() as e:
+			exec(request.args.get('cmd'))
+		return str(e.getvalue())
+	except Exception as e:
+		return e
+app.run(host='0.0.0.0',port=int(os.environ.get('PORT','5000')), debug=True)
